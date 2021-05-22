4dad30240e8185a7fca94a7c7724ca344f777ef8
douglasbpl <douglas.bpl94@yahoo.com.br>
2021-05-21 23:11:42 -0300

    pratica 4 de lógica de programação

---
+++ index.js
@@ -0,0 +1,38 @@
+
+// 1 - crie uma função que exiba uma mensagem no console
+
+function msg(){
+    console.log("Meu aniversário está chegando")
+
+}
+msg()
+
+// 2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+function nome(name){
+    console.log(`Oi me chamo ${name}`)
+}
+console.log("Douglas")
+
+// 3 - crie uma função que receba seu nome, idade, e estilo musical preferido (parâmetros) e exiba no console
+
+function eu(nome,idade,estiloMusicalPreferido ){
+console.log(`Meu nome é ${nome},tenho ${idade} e gosto muito de músicas do estilo musical ${estiloMusicalPreferido}`)
+}
+eu("Douglas",26,"Pop")
+
+
+// 4 - crie uma função que receba o seu filme favorito, musica favorita (parâmetros) e exiba no console
+
+function euNovamente(filmeFavorito,musicaFavorita){
+    console.log(`Adoro o filme "${filmeFavorito}" e a música "${musicaFavorita}"`)
+}
+euNovamente("As Branquelas" , "A Thousand Miles")
+
+// 5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+
+function triploX(x){
+    return x*3
+}
+
+console.log(triploX(60))
